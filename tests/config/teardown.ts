12e06eedf35fc40c68662d92d01713a45e8e0cdb
Stewart Allen <stewart.allen@gmail.com>
2024-02-12 19:24:35 -0500

    fix: change test teardown to .ts to access module data

--- tests/config/teardown.ts
+++ tests/config/teardown.ts
@@ -1,7 +1,4 @@
-// import { TEST_DDIR } from '../testConstants';
-// import { fs } from 'fs-extra';
-
-const fs = require('fs-extra');
+import { TEST_DDIR } from '../testConstants';
 
 module.exports = function teardown() {
     // eslint-disable-next-line no-undef
@@ -11,5 +8,5 @@ module.exports = function teardown() {
         console.log({ teardown_error: error });
     }
     // this SHOULD come from testConstants, but we can't import here (yet)
-    fs.remove('test-data');
+    require('fs-extra').remove(TEST_DDIR);
 };
