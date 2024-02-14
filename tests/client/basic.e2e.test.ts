fee988fab89c84b2bddb38191acebebf801c076c
courthive <charles@courthive.com>
2024-02-13 19:48:17 -0500

    refactor: :truck: relocate client tests

--- tests/client/basic.e2e.test.ts
+++ tests/client/basic.e2e.test.ts
@@ -1,14 +1,14 @@
-import { TEST_PORT, CLIENT_TESTS } from 'tests/testConstants';
+import { CLIENT_TESTS } from 'tests/common/clientCoverage';
+import { TEST_PORT } from 'tests/testConstants';
+
 import netLevelClient from 'lib/client.js';
 
 describe('test harness for net-level-client', () => {
-
     const client = new netLevelClient();
 
     test('open', async () => {
         await client.open('localhost', TEST_PORT);
     });
 
-    CLIENT_TESTS({ client }, "e2e");
-
+    CLIENT_TESTS({ client }, 'e2e');
 });
